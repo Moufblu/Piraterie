8f7542a45aa877cc2b845a2fdc51384156dcfa62
MiguelPomboDias <miguel.pombodias@heig-vd.ch>
2017-06-05 16:29:55 +0200

    Ajout methodes dans abstract

--- src/main/java/mediators/AbstractMediator.java
+++ src/main/java/mediators/AbstractMediator.java
@@ -1,6 +1,46 @@
 package mediators;
 
-public abstract class AbstractMediator
-{
+import java.awt.Point;
+import java.util.List;
+import ships.Attacker;
+import ships.BlackBeard;
+import ships.Corsair;
+import ships.King;
+import ships.Merchant;
+import ships.Pirate;
+import ships.Ship;
+
+public abstract class AbstractMediator {
+
+   protected List<Corsair> corsairs;
+   protected List<Merchant> merchants;
+   protected List<Pirate> pirates;
+   protected BlackBeard blackBeard;
+   protected King king;
+
+   static GreatMediator greatMediator;
+
+   static {
+      greatMediator = new GreatMediator();
+   }
+
+   public AbstractMediator(List<Corsair> corsairs, List<Merchant> merchants, List<Pirate> pirates) {
+      this.corsairs = corsairs;
+      this.merchants = merchants;
+      this.pirates = pirates;
+      
+   }
    
+   
+   
+   double getSpeedModifier() {
+      return 1.0;
+   }
+
+   abstract void move(Ship ship);
+
+   abstract void wantToAttack(Attacker attacker);
+
+   abstract void returnToBase(Point base);
+
 }
