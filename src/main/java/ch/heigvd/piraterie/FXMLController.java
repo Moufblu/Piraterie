caccc33f69208849d6b9599f9d69f36b2fd45611
Edward Ransome <edward.ransome@heig-vd.ch>
2017-06-14 16:18:44 +0200

    interface

--- src/main/java/ch/heigvd/piraterie/FXMLController.java
+++ src/main/java/ch/heigvd/piraterie/FXMLController.java
@@ -12,11 +12,6 @@ public class FXMLController implements Initializable {
     @FXML
     private Label label;
     
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
