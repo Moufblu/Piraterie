a8747dbec729346f0d77e92e311b730d5fc78c8d
Michael Spierer <evondom@hotmail.com>
2017-06-14 19:02:25 +0200

    attack implémenté

--- src/main/java/mediators/GreatMediator.java
+++ src/main/java/mediators/GreatMediator.java
@@ -2,89 +2,126 @@ package mediators;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import ships.BlackBeard;
 import ships.Corsair;
 import ships.King;
 import ships.Merchant;
 import ships.Pirate;
 import ships.Ship;
+
 import java.util.LinkedList;
 import java.util.Optional;
+
 import utils.Point;
 
 public class GreatMediator {
 
-   public enum ShipType {
+    public enum ShipType {
 
-      CORSAIR {
-         public Point getDepositPosition() {
-            return new Point(0, 0);
-         }
-      }, MERCHANT {
-         public Point getDepositPosition() {
-            return new Point(0, 0);
-         }
-      }, PIRATE {
-         public Point getDepositPosition() {
-            return new Point(0, 0);
-         }
-      };
+        CORSAIR {
+            public Point getDepositPosition() {
+                return new Point(0, 0);
+            }
+        }, MERCHANT {
+            public Point getDepositPosition() {
+                return new Point(0, 0);
+            }
+        }, PIRATE {
+            public Point getDepositPosition() {
+                return new Point(0, 0);
+            }
+        };
 
-      public abstract Point getDepositPosition();
-   }
+        public abstract Point getDepositPosition();
+    }
 
-   List<AbstractMediator> mediators;
-   List<List<Ship>> ships;
+    List<AbstractMediator> mediators;
+    List<List<Ship>> ships;
 
-   AbstractMediator[][] mediatorMatrix;
+    AbstractMediator[][] mediatorMatrix;
 
-   private BlackBeard blackBeard;
-   private King king;
+    private BlackBeard blackBeard;
+    private King king;
 
-   public GreatMediator(List<AbstractMediator> mediators) {
-      this.ships = new ArrayList<>(ShipType.values().length);
+    public GreatMediator(List<AbstractMediator> mediators) {
+        this.ships = new ArrayList<>(ShipType.values().length);
 
-      ships.stream().forEach((ship) -> {
-         ship = new LinkedList<>();
-      });
-   }
+        ships.stream().forEach((ship) -> {
+            ship = new LinkedList<>();
+        });
+    }
 
-   public Optional<Ship> getClosest(Ship s, ShipType type) {
-      return ships.get(type.ordinal())
-              .stream()
-              .sorted((a, b) -> a.distanceTo(s) - b.distanceTo(s))
-              .findFirst();
-   }
+    public Optional<Ship> getClosest(Ship s, ShipType type) {
+        return ships.get(type.ordinal())
+                .stream()
+                .sorted((a, b) -> a.distanceTo(s) - b.distanceTo(s))
+                .findFirst();
+    }
 
-   public void add(Pirate p) {
-      ships.get(ShipType.PIRATE.ordinal()).add(p);
-   }
+    public void add(Pirate p) {
+        ships.get(ShipType.PIRATE.ordinal()).add(p);
+    }
 
-   public void add(Merchant m) {
-      ships.get(ShipType.MERCHANT.ordinal()).add(m);
-   }
+    public void add(Merchant m) {
+        ships.get(ShipType.MERCHANT.ordinal()).add(m);
+    }
 
-   public void add(Corsair c) {
-      ships.get(ShipType.CORSAIR.ordinal()).add(c);
-   }
+    public void add(Corsair c) {
+        ships.get(ShipType.CORSAIR.ordinal()).add(c);
+    }
 
-   public void wantToDeposit(Merchant m) {
-      deposit(m, ShipType.MERCHANT);
-   }
+    public void wantToDeposit(Merchant m) {
+        deposit(m, ShipType.MERCHANT);
+    }
 
-   public void wantToDeposit(Corsair c) {
-      deposit(c, ShipType.CORSAIR);
-   }
+    public void wantToDeposit(Corsair c) {
+        deposit(c, ShipType.CORSAIR);
+    }
 
-   public void wantToDeposit(Pirate p) {
-      deposit(p, ShipType.PIRATE);
-   }
+    public void wantToDeposit(Pirate p) {
+        deposit(p, ShipType.PIRATE);
+    }
 
-   public void deposit(Ship s, ShipType st) {
-      Point p = s.getPosition();
+    public void deposit(Ship s, ShipType st) {
+        for (int i = 0; i < s.getSpeed(); i++) {
+            move(s, st.getDepositPosition());
+        }
+    }
 
-      for (int i = 0; i < s.getSpeed(); i++) {
-         mediatorMatrix[p.getX()][p.getY()].move(s, st.getDepositPosition());
-      }
-   }
+    //Tell the corresponding mediator to move ship in the direction of point
+    public void move(Ship ship, Point point) {
+        mediatorMatrix[ship.getPosition().getX()][ship.getPosition().getY()].move(ship, point);
+    }
+
+    /**
+     * @param s the ship attacking
+     * @param shipToAttack the ship to attack
+     * @return true if s and shipToAttack are in range
+     */
+    public boolean isInRange(Ship s, Ship shipToAttack){
+        return mediatorMatrix[s.getPosition().getX()][s.getPosition().getY()].isInRange(s,shipToAttack);
+    }
+
+
+    public void attack(Ship s, ShipType shipTypeToAttack) {
+        Optional<Ship> shipToAttackOpt = getClosest(s, shipTypeToAttack);
+        for (int i = 0; i < s.getSpeed(); i++) {
+            if (shipToAttackOpt.isPresent()) {
+                Ship shipToAttack = shipToAttackOpt.get();
+                //S'il est a porté, on l'attaque
+                if (isInRange(s,shipToAttack)) {
+                    //attaque une fois et passe son tour
+                    mediatorMatrix[s.getPosition().getX()][s.getPosition().getY()].attack(s,shipToAttack);
+                    break;
+                } else { //Sinon on se dirige vers lui
+                    move(s,shipToAttack.getPosition());
+                }
+            } else {
+                System.out.println("No ship to attack !");
+                //move to a random position
+                move(s);
+            }
+        }
+    }
 }
